0ef59e7647e8c31221ab0e0e83abb400f4b9c7b4
Adam Petrie <adam.petrie@wishabi.com>
2016-05-25 13:32:36 -0400

    Fix error on big map when waterparks object is not defined.

--- _assets/js/waterparks.js
+++ _assets/js/waterparks.js
@@ -1,25 +1,26 @@
-/* global 
-    google
-*/
 (function ($) {
     'use strict';
 
-  $('#big-map').gmap3({
-      marker: {
-          values: waterparks,
-          events: {
-            click: function(marker, event, context) {
-              window.location.href= context.data.url;
+  var $big_map = $('#big-map');
+
+  if ($big_map.length) {
+    $big_map.gmap3({
+        marker: {
+            values: waterparks,
+            events: {
+              click: function(marker, event, context) {
+                window.location.href = context.data.url;
+              },
             },
-          },
-      },
-      autofit:{},
-      map: {
-          options: {
-              mapTypeId: google.maps.MapTypeId.ROADMAP,
-              zoom: 7,
-              scrollwheel: false
-          }
-      }
-  });
+        },
+        autofit:{},
+        map: {
+            options: {
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                zoom: 7,
+                scrollwheel: false
+            }
+        }
+    });
+  }
 })(jQuery);
